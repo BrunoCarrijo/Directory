51fadfaceeee216152337a21c546dca59fe747f8
Bruno Carrijo <bruno.carrijo1009@gmail.com>
2021-12-01 19:31:41 -0300

    Usando Directory para criare pasta e mostrar arquivos existentes

---
+++ _Directory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _Directory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"d:\C#\myfolder";
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, " *.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                Directory.CreateDirectory(@"d:\C#\myfolder\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
